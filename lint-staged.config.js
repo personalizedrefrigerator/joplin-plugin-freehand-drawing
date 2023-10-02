2f3d91869c98fc7096a26608cd4ea32fa2a202da
Henry Heino <personalizedrefrigerator@gmail.com>
2023-10-02 15:31:27 -0700

    build: Configure repository for use with prettier

---
+++ lint-staged.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	// --ignore-path tells prettier to ignore files in .eslintignore
+	'**/*.{js,ts,tsx}': ['eslint --fix', 'prettier --ignore-path .eslintignore --write'],
+	'**/*.{md,css,scss}': ['prettier --ignore-path .eslintignore --write'],
+};
