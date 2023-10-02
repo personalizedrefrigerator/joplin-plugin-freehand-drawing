3629a03da1add85cedd23c2229d232d1796d3954
Henry Heino <personalizedrefrigerator@gmail.com>
2023-10-02 16:07:04 -0700

    chore: Fix git pre-commit hook not correctly configured

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,5 @@
 module.exports = {
 	// --ignore-path tells prettier to ignore files in .eslintignore
 	'**/*.{js,ts,tsx}': ['eslint --fix', 'prettier --ignore-path .eslintignore --write'],
-	'**/*.{md,css,scss}': ['prettier --ignore-path .eslintignore --write'],
+	'**/*.{json,md,css,scss}': ['prettier --ignore-path .eslintignore --write'],
 };
