efa5fa046c08cea807596ea79f0ae22f1f45033a
Henry Heino <personalizedrefrigerator@gmail.com>
2023-11-13 12:42:40 -0800

    Make screenshot paths relative to the repository root

--- webpack.config.js
+++ webpack.config.js
@@ -101,7 +101,7 @@ function validateScreenshots(screenshots) {
 		const screenshotType = screenshot.src.split('.').pop();
 		if (!allPossibleScreenshotsType.includes(screenshotType)) throw new Error(`${screenshotType} is not a valid screenshot type. Valid types are: \n${allPossibleScreenshotsType}\n`);
 
-		const screenshotPath = path.resolve(srcDir, screenshot.src);
+		const screenshotPath = path.resolve(rootDir, screenshot.src);
 		// Max file size is 1MB
 		const fileMaxSize = 1024;
 		const fileSize = fs.statSync(screenshotPath).size / 1024;
